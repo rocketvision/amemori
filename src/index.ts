23721ef9182a0103050d0ef5a96c0bf487fa7419
Lord <10253775+lewdum@users.noreply.github.com>
2022-01-15 01:49:09 -0300

    index: new modules

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import * as api from './api'
 import * as form from './form'
+import * as args from './args'
+import * as codes from './codes'
+import * as errors from './errors'
+import * as lang from './lang'
 
 export default {
   api,
   form,
+  args,
+  codes,
+  errors,
+  lang,
 }
